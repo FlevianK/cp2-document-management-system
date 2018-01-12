926ebc0c0e89276b8e9538c2c4717044293fcb36
FlevianK <flevian.kanaiza@andela.com>
2018-01-12 13:04:12 +0300

    change backend port from 8000 to 3000 for vagrant

--- client/src/api/documentApi.js
+++ client/src/api/documentApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://dms-flev-backend.herokuapp.com/api' || 'http://localhost:8000/api'; // comment this code when running this app locally
-// const API_URL = 'http://localhost:8000/api'; // comment this code if pushing this file to git
+// const API_URL = 'https://dms-flev-backend.herokuapp.com/api' || 'http://localhost:8000/api'; // comment this code when running this app locally
+const API_URL = 'http://localhost:3000/api'; // comment this code if pushing this file to git
 
 export const allDocuments = () => {
   const token = localStorage.jwt;
@@ -87,7 +87,7 @@ export const documentSearchPage = (documentValue, limit, offset) => {
   );
 };
 
-export const allRoleDocument = () =>{
+export const allRoleDocument = () => {
   const token = localStorage.jwt;
   return axios.get(
     `${API_URL}/roles/documents`,
--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://dms-flev-backend.herokuapp.com/api' || 'http://localhost:8000/api'; // comment this code when running this app locally
-// const API_URL = 'http://localhost:8000/api'; // comment this code if pushing this file to git
+// const API_URL = 'https://dms-flev-backend.herokuapp.com/api' || 'http://localhost:8000/api'; // comment this code when running this app locally
+const API_URL = 'http://localhost:3000/api'; // comment this code if pushing this file to git
 
 export const login = (user) => {
   return axios.post(`${API_URL}/users/login`, user);
